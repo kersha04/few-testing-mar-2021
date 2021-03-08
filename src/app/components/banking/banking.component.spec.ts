21cd5f05be23aa50dce3162a7444008c5e9cf079
Jeffry Gonzalez <jeff@hypertheory.com>
2021-03-08 16:34:59 -0500

    done with banking component test

--- src/app/components/banking/banking.component.spec.ts
+++ src/app/components/banking/banking.component.spec.ts
@@ -2,12 +2,14 @@ import { async, ComponentFixture, TestBed, waitForAsync } from '@angular/core/te
 import { By } from '@angular/platform-browser';
 import { BankAccountService } from 'src/services/bank-account.service';
 import { BankingComponent } from './banking.component';
+import { FixtureElementUtils } from '../../utils/testing';
 
 describe('BankingComponent', () => {
 
   let component: BankingComponent;
   let fixture: ComponentFixture<BankingComponent>;
   let bankingServiceSpy: jasmine.SpyObj<BankAccountService>;
+  let fixtureElementUtils: FixtureElementUtils;
 
   beforeEach(() => {
     bankingServiceSpy = jasmine.createSpyObj<BankAccountService>(
@@ -25,6 +27,7 @@ describe('BankingComponent', () => {
 
   beforeEach(() => {
     fixture = TestBed.createComponent(BankingComponent);
+    fixtureElementUtils = new FixtureElementUtils(fixture.debugElement);
     component = fixture.componentInstance;
     bankingServiceSpy.getBalance.and.returnValue(42);
     fixture.detectChanges();
@@ -33,9 +36,10 @@ describe('BankingComponent', () => {
   describe('intitialization', () => {
     it('has the appropriate balance displayed', () => {
 
-      const bankingDe = fixture.debugElement;
-      const bonusDe = bankingDe.query(By.css('[data-t-banking-balance]'));
-      const bonusEl = bonusDe.nativeElement as HTMLSpanElement;
+      // const bankingDe = fixture.debugElement;
+      // const bonusDe = bankingDe.query(By.css('[data-t-banking-balance]'));
+      // const bonusEl = bonusDe.nativeElement as HTMLSpanElement;
+      const bonusEl = fixtureElementUtils.getNativeElement<HTMLElement>('[data-t-banking-balance]');
       expect(bonusEl.textContent).toEqual('$42.00');
     });
 
@@ -43,6 +47,30 @@ describe('BankingComponent', () => {
   });
   describe('making deposits', () => {
 
+    beforeEach(() => {
+      const amountEl = fixtureElementUtils.getNativeElement<HTMLInputElement>('[data-t-banking-amount-input]');
+      amountEl.value = '42';
+
+      const depositButton = fixtureElementUtils.getNativeElement<HTMLButtonElement>('[data-t-banking-deposit-button]');
+      bankingServiceSpy.getBalance.and.returnValue(99);
+      depositButton.click();
+      fixture.detectChanges();
+
+    });
+    it('should call the service with the right amount', () => {
+      expect(bankingServiceSpy.deposit).toHaveBeenCalledOnceWith(42);
+    });
+
+    it('should get the balance from the service and display it', () => {
+      const bonusEl = fixtureElementUtils.getNativeElement<HTMLElement>('[data-t-banking-balance]');
+      expect(bonusEl.textContent).toEqual('$99.00');
+      // it also proves we used the currency pipe!
+    });
+
+    it('should clear out the input', () => {
+      const amountEl = fixtureElementUtils.getNativeElement<HTMLInputElement>('[data-t-banking-amount-input]');
+      expect(amountEl.value).toBe('');
+    });
 
   });
   describe('making withdrawals', () => {
