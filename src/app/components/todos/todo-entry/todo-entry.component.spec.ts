0d26ffab1c7bd83b40992b3e61af8d4fbff2bddf
Jeffry Gonzalez <jeff@hypertheory.com>
2021-03-09 15:54:24 -0500

    done with the todo-entry component

---
+++ src/app/components/todos/todo-entry/todo-entry.component.spec.ts
@@ -0,0 +1,40 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { TodoEntryComponent } from './todo-entry.component';
+import { FixtureElementUtils } from '../../../utils/testing';
+describe('TodoEntryComponent', () => {
+
+  let componentFixture: ComponentFixture<TodoEntryComponent>;
+  beforeEach(() => {
+
+    TestBed.configureTestingModule({
+      declarations: [TodoEntryComponent],
+      imports: [ReactiveFormsModule]
+    });
+
+    componentFixture = TestBed.createComponent(TodoEntryComponent);
+    componentFixture.componentInstance.ngOnInit();
+    componentFixture.detectChanges();
+  });
+
+  it('the form should not be valid until it filled out', () => {
+    expect(componentFixture.componentInstance.form.invalid).toBe(true);
+    const errors = componentFixture.componentInstance.item.errors;
+    expect(errors).toEqual({
+      required: true
+    });
+  });
+
+  it('emits when the form is valid and submitted', () => {
+    const util = new FixtureElementUtils(componentFixture.debugElement);
+    const submitButton = util.getNativeElement<HTMLButtonElement>('[data-t-todo-submit]');
+    spyOn(componentFixture.componentInstance.itemAdded, 'emit');
+    componentFixture.componentInstance.item.setValue('Things');
+    submitButton.click();
+    componentFixture.detectChanges();
+    expect(componentFixture.componentInstance.form.valid).toBe(true);
+    expect(componentFixture.componentInstance.itemAdded.emit)
+      .toHaveBeenCalledOnceWith('Things');
+
+  });
+});
